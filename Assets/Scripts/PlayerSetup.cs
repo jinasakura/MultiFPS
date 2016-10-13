a1e527c3bb49fe288383d1b4d4c331b801ca959e
jinasakura <jinasakura@163.com>
2016-10-13 12:20:39 +0800

    提交之前没有提交的

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -29,31 +29,9 @@ public class PlayerSetup : NetworkBehaviour {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //问题3：这里个函数的作用让我费解
-    //可能会调用这个函数的时候，就是Start里当“我”控制的Player时，禁用主摄像机
-    //可是当“我”这个Player死后，才会调用主摄像机，那现在刚禁用了现在又恢复是怎么回事
-    //并且这个代码注释掉并没有影响其他功能
     void OnDisable()
     {
-        if (sceneCamera != null)
-        {
-            sceneCamera.gameObject.SetActive(true);
-        }
+        sceneCamera.gameObject.SetActive(true);
     }
 
 }
